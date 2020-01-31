389ccb88fde375fc6c944b39f72a662a765f4600
rosemarie betrong <rmbt.lab@gmail.com>
2020-01-30 20:27:09 -0500

    complete rating on index to be added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,12 @@
 class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
+    @reviews = Review.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @average = average(@restaurant)
   end
 
   # CREATE
@@ -17,10 +19,22 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
-    redirect_to restaurants_path
+    redirect_to restaurant_path(@restaurant)
   end
 
-private
+  def average(restaurant)
+    if restaurant.reviews.count.zero?
+      'not rated'
+    else
+      sum = 0
+      restaurant.reviews.each do |review|
+        sum += review.rating
+      end
+      sum / restaurant.reviews.count
+    end
+  end
+
+  private
 
   # STRONG PARAMS
   def restaurant_params
